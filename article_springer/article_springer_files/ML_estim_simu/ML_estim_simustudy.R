9e597b2c1a4a62e3ee6d38eebbac9c8899bee9f2
Katharina Hees <HeesKatharina@gmail.com>
2020-03-26 12:08:55 +0100

    Überarbeitung, Finale Änderung bzgl Reviewer 2

--- article_springer/article_springer_files/ML_estim_simu/ML_estim_simustudy.R
+++ article_springer/article_springer_files/ML_estim_simu/ML_estim_simustudy.R
@@ -70,10 +70,4 @@ reshape2::melt(estimates) %>%
 
 save(tbl_ML_estim,file="tbl_ML_estim.RData")
 
-# RMSE
 
-tbl_ML_estim %>%
-  ggplot(aes(true.tail,rmse, colour = method)) +
-  geom_line() + geom_point() +
-  facet_grid(param ~ n, scales = "free_y") +
-  scale_colour_brewer("method", palette = "Set1")
